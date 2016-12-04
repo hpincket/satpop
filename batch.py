1c1913ee2f016ef92839088eb2783d46e5cb4cf9
danwang1995 <danwang1995@gmail.com>
2016-12-04 18:57:30 -0500

    Batcher with semicontinuous probability distribution

--- batch.py
+++ batch.py
@@ -123,8 +123,41 @@ class SatPopBatch:
 
     def __make_one_hot(self, dense_labels, max):
         one_hots = np.zeros((len(dense_labels), self.label_transformer.number_of_labels()))
-        for i, val in enumerate(dense_labels):
-            one_hots[i, val] = 1
+        
+        if max == 3:
+            y_0 = [0.75, 0.25, 0.0] # rural
+            y_1 = [0.25, 0.5, 0.25] # surburban
+            y_2 = [0.0, 0.25, 0.75] # urban
+            
+            for i, val in enumerate(dense_labels):
+                if val == 0:
+                    one_hots[i] = y_0
+                elif val == 1:
+                    one_hots[i] = y_1
+                elif val == 2:
+                    one_hots[i] = y_2
+        elif max == 5:
+            y_0 = [0.75, 0.2, 0.05, 0.0, 0.0] # unpopulated
+            y_1 = [0.25, 0.5, 0.2, 0.05, 0.0] # rural
+            y_2 = [0.05, 0.2, 0.5, 0.2, 0.05] # suburban
+            y_3 = [0.0, 0.05, 0.2, 0.5, 0.25] # urban
+            y_4 = [0.0, 0.0, 0.05, 0.2, 0.75] # highly urban
+            
+            for i, val in enumerate(dense_labels):
+                if val == 0:
+                    one_hots[i] = y_0
+                elif val == 1:
+                    one_hots[i] = y_1
+                elif val == 2:
+                    one_hots[i] = y_2
+                elif val == 3:
+                    one_hots[i] = y_3
+                elif val == 4:
+                    one_hots[i] = y_4
+        else:
+            for i, val in enumerate(dense_labels):
+                one_hots[i, val] = 1
+        
         return one_hots
 
     def __iter__(self):
